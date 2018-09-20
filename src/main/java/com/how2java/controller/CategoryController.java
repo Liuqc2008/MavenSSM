995827004a68be8570ac6f08045afec7a3db2453
Liuqc2008 <313624981@qq.com>
2018-09-20 10:04:01 +0800

    去掉modelandview

--- src/main/java/com/how2java/controller/CategoryController.java
+++ src/main/java/com/how2java/controller/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,34 +23,27 @@ public class CategoryController {
 	CategoryService categoryService;
 
 	@RequestMapping("listCategory")
-	public ModelAndView listCategory(Page page){
-		ModelAndView mav = new ModelAndView();
+	public String listCategory(Page page,Model model){
+	
 		PageHelper.offsetPage(page.getStart(),5);
 		List<Category> cs= categoryService.list();
 		int total = (int) new PageInfo<>(cs).getTotal();
 		
 		page.caculateLast(total);
 		
-		// 放入转发参数
-		mav.addObject("cs", cs);
-		// 放入jsp路径
-		mav.setViewName("jsp/listCategory");
-		return mav;
+		model.addAttribute("cs", cs);
+		return "jsp/listCategory";
 	}
 
 	@RequestMapping("form")
-	public ModelAndView form(){
-		ModelAndView mav = new ModelAndView();
-		
-		mav.setViewName("jsp/form");
-		return mav;
+	public String form(){
+
+		return "jsp/form";
 	}
 	
 	@RequestMapping("layui")
-	public ModelAndView layui(){
-		ModelAndView mav = new ModelAndView();
-		
-		mav.setViewName("jsp/layui");
-		return mav;
+	public String layui(){
+
+		return "jsp/layui";
 	}
 }
